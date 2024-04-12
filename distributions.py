5f6d1c7996f97bbeec1c0857c9647bddc4af0a44
Martin <martin.rivard@teksavvy.com>
2024-04-11 21:38:09 -0400

    Add textbook's normal variate transform.

--- distributions.py
+++ distributions.py
@@ -1,6 +1,10 @@
 import random
 import math
 
+def genU():
+    #generate uniform from 0-1
+    return (float(random.randrange(10000))/10000)
+
 class Distribution:
     def __init__(self):
         pass
@@ -17,17 +21,17 @@ class DistUniform(Distribution):
         self.start = start
         self.end = end
     def genNumber(self):
-        return ((float(random.randrange(10000))/10000) * (self.end-self.start)) + self.start
+        return (genU() * (self.end-self.start)) + self.start
         
 class DistExponential(Distribution):
     def __init__(self, average):
         Distribution.__init__(self)
         self.lambdaVal = 1.0/float(average)
     def genNumber(self):
-        return math.log( 1- (float(random.randrange(10000))/10000))/(self.lambdaVal*-1)
+        return math.log( 1 - genU())/(self.lambdaVal*-1)
         
 class DistBernoulli(Distribution):
-    #technically this is the geometric distribution
+    #geometric distribution, not bernoulli
     def __init__(self, failureChance):
         # chance: float from 0-1
         Distribution.__init__(self)
@@ -36,11 +40,12 @@ class DistBernoulli(Distribution):
     def genNumber(self):
         return self.genInt()
         
+    #perform trials until a failure occurs
     def genInt(self):
         n = 0
         failed = 1
         while(failed):
-            if (float(random.randrange(10000))/10000) <= self.failureChance:
+            if genU() <= self.failureChance:
                 failed = 0
             else:
                 n += 1
@@ -57,10 +62,11 @@ class DistBinomial(Distribution):
     def genNumber(self):
         return self.genInt()
         
+    #perform nTrials trials, and tally up number of successes
     def genInt(self):
         n = 0
         for i in range(self.nTrials):
-            if (float(random.randrange(10000))/10000) <= self.chance:
+            if genU() <= self.chance:
                 n += 1
         return n
         
@@ -72,4 +78,12 @@ class DistNormal(Distribution):
         self.stdev = math.sqrt(float(variance))
         
     def genNumber(self):
-        return random.normalvariate(self.mean, self.stdev)
+        u1 = genU()
+        if u1 == 0:
+            u1 = genU()
+        u2 = genU()
+        # print(f'u1: {u1}')
+        z = math.sqrt((-2 * math.log(u1))) * math.cos(2 * math.pi * u2)
+        return (self.mean + (z * self.stdev))
+    #def genNumber(self):
+     #   return random.normalvariate(self.mean, self.stdev)
